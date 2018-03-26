5e50390824e9a6cdf579d6744ee6ca3b50295c66
Justin Merz <jrmerz@gmail.com>
2018-03-26 08:37:52 -0700

    moving elements to root, removing build

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+import "@polymer/polymer/polymer-element"
 import "@polymer/iron-media-query"
 import "@polymer/iron-pages"
--- webpack-dist.config.js
+++
@@ -1,8 +0,0 @@
-let config = require('@ucd-lib/cork-app-build').dist({
-  root : __dirname,
-  entry : 'elements/index.js',
-  dist : 'dist',
-  clientModules : 'elements/node_modules'
-});
-
-module.exports = config;
