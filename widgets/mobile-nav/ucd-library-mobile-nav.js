f7f4b85b14d725840702d6be4c13c88288b5678f
Steve Pelkey <spelkey@ucdavis.edu>
2019-04-12 16:57:16 -0700

    track fetches

--- widgets/mobile-nav/ucd-library-mobile-nav.js
+++ widgets/mobile-nav/ucd-library-mobile-nav.js
@@ -2,7 +2,8 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import "@ucd-lib/cork-app-utils/lib/Mixin.js"
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import '@polymer/iron-ajax/iron-ajax.js';
-import '@polymer/iron-media-query/iron-media-query.js';
+//import '@polymer/iron-media-query/iron-media-query.js';
+import '@polymer/iron-pages/iron-pages.js';
 
 import template from "./template.js";
 import style from "./style.js";
@@ -22,8 +23,8 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
         type: Boolean
       },
       library_url: {
-          type: String,
-          value: "https://www.library.ucdavis.edu"
+        type: String,
+        value: "https://www.library.ucdavis.edu"
       },
       api_base_url: {
         type: String,
@@ -36,7 +37,46 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
       api_pages_url: {
         type: String,
         value: "/wp/v2/pages"
-      }
+    },
+      look_ahead: {
+        type: Number,
+        value: 1
+    },
+      menu_data: {
+        type: Array,
+        value: []
+    },
+      selected_menu: {
+        type: Object,
+        value: {}
+    },
+      next_menu: {
+        type: Object,
+        value: {}
+    },
+      animation_duration: {
+        type: String,
+        value: "2s"
+    },
+      menu_loaded: {
+        type: Boolean
+    },
+      menus_to_retrieve: {
+        type: Object
+    },
+      n_menus_to_retrieve: {
+        type: Number,
+        computed: "_n_menus_to_retrieve(menus_to_retrieve)"
+    },
+      menus_retrieved: {
+        type: Array,
+        value: []
+    },
+      all_menus_retrieved: {
+        type: Boolean,
+        computed: "_all_menus_retrieved(menus_retrieved.*)"
+    }
+
     };
   }
 
@@ -48,18 +88,31 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
       super.ready();
       if (this.verbose) {
         console.log("Mobile nav widget loaded.");
+      }
 
+      // Retrieve official wp menus
+      let menus = {'main-nav':['library-locations', 'alumni-and-friends', 'about'],
+                   'info': ['lang-prize']};
+      this.set('menus_to_retrieve', menus);
+      this.$.ajax_descendents.url = this.library_url + this.api_base_url + this.api_pages_url;
+      let menu = [];
+      //let menu = this.get_wp_menu('main-nav');
+      menu = menu.concat(this.get_wp_menu('main-nav'))
+      menu = menu.concat(this.get_wp_menu('info'));
+      //this.get_wp_menu('info');
+      this.set('menu_data', menu);
+
+
+      // Check if current page is hierarchical
+      // Get parents(all) and children of page
+      if (this.verbose) {
         if (typeof WP_POST_ID !== 'undefined'){
             console.log("Page id:", WP_POST_ID);
         }
         if (typeof WP_POST_TYPE !== 'undefined'){
             console.log("Page type:", WP_POST_TYPE);
         }
-
       }
-      this.$.ajax_descendents.url = this.library_url + this.api_base_url + this.api_pages_url;
-      this.set('menu_main', this.get_wp_menu('main-nav'));
-      //this.set('menu_info', this.get_wp_menu('info'));
 
   }
 
@@ -74,34 +127,30 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
       request_parent.completes.then(function(req){
           var response = req.response;
           for (var link of response.items) {
-              let link_filtered = element._parse_menu_item(link)
-              output.push(link_filtered)
-          }
-
-          // determine submenus to query based on parent menu
-          let submenus = [];
-          if (menu == 'main-nav') {
-              submenus = ['library-locations', 'alumni-and-friends', 'about'];
-          }
-          else if (menu == 'info') {
-              submenus = ['lang-prize'];
+              let link_filtered = element._parse_menu_item(link);
+              if (menu == 'info') {
+                  link_filtered['link_style']  = "menu_info";
+              }
+              output.push(link_filtered);
           }
 
           // Fetch and combine the various submenus of the parent menu
           // First attempt to match on page id, then the label
+          let submenus = element.menus_to_retrieve[menu];
           for (var submenu of submenus) {
               element.$.ajax_menu.url = element.menu_url(submenu);
               let request_child = element.$.ajax_menu.generateRequest();
               request_child.completes.then(function(child_req){
                   var child_response = child_req.response;
                   let parent = child_response.items.shift();
+                  parent = element._parse_menu_item(parent)
                   var in_output = false;
                   for (var i = 0; i < output.length; i++) {
-                      if ( (output[i]['id'] == parent['object_id']) && (output[i]['object'] == parent['object']) ) {
+                      if ( (output[i]['id'] == parent['id']) && (output[i]['object'] == parent['object']) ) {
                           in_output = true;
                           break;
                       }
-                      else if ( output[i]['label'] == parent['title'] ) {
+                      else if ( output[i]['label'] == parent['label'] ) {
                           in_output = true;
                           break;
                       }
@@ -109,15 +158,16 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
                   if (in_output == true) {
                       for (var child_link of child_response.items) {
                           let child_link_filtered = element._parse_menu_item(child_link);
-                          if ((child_link_filtered.object == 'page') && (submenu != 'library-locations')) {
-                              child_link_filtered['children'] = element.get_page_descendents(child_link_filtered['id']);
-                              child_link_filtered['retrieved_children'] = true;
-                          }
+                          //if ((child_link_filtered.object == 'page') && (submenu != 'library-locations')) {
+                        //      child_link_filtered['children'] = element.get_page_descendents(child_link_filtered['id']);
+                        //      child_link_filtered['retrieved_children'] = true;
+                        //  }
 
                           output[i]['children'].push(child_link_filtered)
                           output[i]['retrieved_children'] = true;
                       }
                   }
+                  element.push("menus_retrieved", [menu, submenu]);
 
               }, function(rejected) {}
           )
@@ -172,10 +222,11 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
       output['id'] = item.object_id;
       output['object'] = item.object;
       output['link'] = item.url;
-      output['label'] = item.title;
+      output['label'] = this._decodeHtml(item.title);
       output['order'] = item.menu_order;
       output['children'] = [];
       output['retrieved_children'] = false;
+      output['link_style'] = "parent";
 
       return output;
   }
@@ -188,8 +239,36 @@ class UCDLibraryMobileNav extends Mixin(PolymerElement)
       output['link'] = item['title']['rendered'];
       output['children'] = [];
       output['retrieved_children'] = false;
+      output['link_style'] = "standard";
 
       return output;
+  }
+
+  _decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+  }
+  _all_menus_retrieved(menus_retrieved) {
+      /* Checks if all the main-nav menus were retrieved */
+      if (this.menus_retrieved.length >= this.n_menus_to_retrieve){
+          if (this.verbose) {
+              console.log("Entire main nav menu retrieved.");
+          }
+          return true
+      }
+      else {
+          return false
+      }
+  }
+  _n_menus_to_retrieve(menus_to_retrieve) {
+      let i = 0;
+      for (var menu in menus_to_retrieve) {
+          if (menus_to_retrieve.hasOwnProperty(menu)) {
+              i += menus_to_retrieve[menu].length
+          }
+      }
+      return i
   }
 
 
